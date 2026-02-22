85fb8d9fb9c9b4aab3d4a3df70e6de52ae1fccbf
Sharofiddin <sharofiddin.abaskhanov@gmail.com>
2026-02-22 12:44:00 +0500

    Add logout feature

---
+++ logout.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def logout(username):
+    """Simple logout function"""
+    print(f"User {username} logged out")
+    return True
+
+if __name__ == "__main__":
+    logout("admin")
